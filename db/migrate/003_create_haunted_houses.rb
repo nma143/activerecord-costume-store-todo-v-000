81550aec50df917268999e1ad9724d2c73e4aa70
Nikki A <nikki.apostolakis@gmail.com>
2018-08-12 17:47:26 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+
+  def change
+     create_table :haunted_houses do |t|
+       t.string :name
+       t.float :location
+       t.integer :costume_inventory
+       t.integer :num_of_employees
+       t.boolean :still_in_business
+       t.datetime :opening_time
+       t.datetime :closing_time
+     end
+   end
+
+end
