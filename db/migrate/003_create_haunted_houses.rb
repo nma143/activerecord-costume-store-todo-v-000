dd5e18a2fb8a92301c732925b0cfe506502238e0
Nikki A <nikki.apostolakis@gmail.com>
2018-08-12 17:50:32 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,12 +4,13 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change
      create_table :haunted_houses do |t|
        t.string :name
-       t.float :location
-       t.integer :costume_inventory
-       t.integer :num_of_employees
-       t.boolean :still_in_business
-       t.datetime :opening_time
-       t.datetime :closing_time
+       t.string :location
+       t.string :theme
+       t.float :price
+       t.boolean :family_friendly
+       t.datetime :opening_date
+       t.datetime :closing_date
+       t.text: description
      end
    end
 
