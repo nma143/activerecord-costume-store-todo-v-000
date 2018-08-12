7c8a565dbd96dc4ef459883eaf6b12a65426d9c0
Nikki A <nikki.apostolakis@gmail.com>
2018-08-12 17:36:24 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,7 +5,7 @@
 # 002_create_costumes.rb becomes class CreateCostumes
 
 # Define a change method in which to do the migration
-# In this change method, create columns with the correct names and 
+# In this change method, create columns with the correct names and
 # value types according to the spec
 
 class CreateCostumes < ActiveRecord::Migration[5.1]
@@ -13,7 +13,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
      create_table :costumes do |t|
        t.string :name
-       t.string :price
+       t.float :price
        t.string :size
        t.string :image_url
      end
